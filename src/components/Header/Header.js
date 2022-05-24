4bc961303404ed101eae9aa330a85d25b0fb838a
esubalew.seife@gmail.com <esubalew.seife@gmail.com>
2022-05-24 10:12:09 -0700

    scroll bar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,23 @@
+import { Fragment } from "react";
 import MainHeading from "../Mainnavigation/MainHeading";
 import MainNavigation from "../Mainnavigation/MainNavigation";
 import SecondNav from "../Mainnavigation/SecondNav";
 import classes from "./Header.module.css";
 const Header = () => {
   return (
-    <header className={classes.header}>
-      <MainNavigation />
-      <SecondNav />
-      <MainHeading />
-    </header>
+    <Fragment>
+      <header className={classes.header}>
+        <MainNavigation />
+        <SecondNav />
+        <MainHeading />
+      </header>
+      <h1>This is body</h1>
+      <h1>This is body</h1>
+      <h1>This is body</h1>
+      <h1>This is body</h1>
+      <h1>This is body</h1>
+      <h1>This is body</h1>
+    </Fragment>
   );
 };
 export default Header;
